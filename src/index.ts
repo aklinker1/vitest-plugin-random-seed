38cd6d3c98e5bd632e3a6b4adb5b827089858395
Aaron <aaronklinker1@gmail.com>
2024-03-15 00:27:35 -0500

    fix: Only log once for workspaces

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import type { Plugin } from 'vite';
 let seed: number;
 
 export default function RandomSeed(options?: RandomSeedPluginOptions) {
+  const shouldLog = seed == null;
   seed ??= options?.seed ?? getEnvSeed() ?? getRandomSeed();
   return {
     name: 'random-seed',
     apply: (_, { mode }) => mode === 'test',
     config: () => {
-      console.log(`Test seed: \x1b[1m\x1b[36m${seed}\x1b[0m`);
+      if (shouldLog) {
+        console.log(`Test seed: \x1b[1m\x1b[36m${seed}\x1b[0m`);
+      }
       return {
         define: {
           'import.meta.test.SEED': JSON.stringify(seed),
